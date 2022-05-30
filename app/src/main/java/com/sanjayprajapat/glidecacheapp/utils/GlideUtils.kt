c478288580deb04286ad094ccb3270f00a49d0e2
sanjaydraws <sprajapat8331@gmail.com>
2022-05-30 23:20:51 +0530

    load image with blur

--- app/src/main/java/com/sanjayprajapat/glidecacheapp/utils/GlideUtils.kt
+++ app/src/main/java/com/sanjayprajapat/glidecacheapp/utils/GlideUtils.kt
@@ -7,6 +7,11 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.sanjayprajapat.glidecacheapp.R
 import jp.wasabeef.glide.transformations.BlurTransformation
+import android.graphics.Bitmap
+import android.util.Log
+
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 
 
 /**
@@ -15,17 +20,21 @@ import jp.wasabeef.glide.transformations.BlurTransformation
  * blog:https://dev.to/sanjayprajapat/
  * */
 
-
+/**
+ * show placeholder when error or image load failed
+ * */
 fun getRequestOption(): RequestOptions {
     val requestOptions = RequestOptions()
         .placeholder(R.drawable.ic_launcher_background)
         .error(R.drawable.drawpic)
     return requestOptions
 }
+
 /**
  * to blur transform
  * */
-fun getBlurRequestOption():RequestOptions = RequestOptions.bitmapTransform( BlurTransformation(25, 5))
+fun getBlurRequestOption():RequestOptions =
+    RequestOptions.bitmapTransform( BlurTransformation(25, 5))
     .error(R.drawable.drawpic)
 
 
@@ -34,7 +43,7 @@ fun getBlurRequestOption():RequestOptions = RequestOptions.bitmapTransform( Blur
  * */
 
 @BindingAdapter("loadImage")
-fun ImageView.loadImage(imageUrl: Any?){
+fun ImageView.loadImage1(imageUrl: Any?){
     imageUrl?.let {
         Glide.with(this).applyDefaultRequestOptions(getBlurRequestOption())
         .load(imageUrl).
@@ -42,6 +51,34 @@ fun ImageView.loadImage(imageUrl: Any?){
         placeholder(R.drawable.ic_launcher_background).
         into(this)
     }
+}
+
+/**
+ * show blur images
+ * */
+@BindingAdapter("loadImage")
+fun ImageView.blurLoadImage(imageUrl: Any?){
+    Glide.with(this)
+        .load(imageUrl)
+        .apply( RequestOptions().centerCrop())
+        .transform( BlurTransformation( 20, 1))
+    .into(this);
+}
+
+/**
+ * show blur image and then show original image
+ * */
+@BindingAdapter("loadImage")
+fun ImageView.loadImage(imageUrl: Any?) {
+    val thumbnailRequest = Glide.with(this)
+        .load(imageUrl).centerCrop()
+        .transform( BlurTransformation( 7, 1))
+
+    Glide.with(this)
+        .load(imageUrl)
+        .thumbnail(thumbnailRequest)
+         .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .into(this)
 }
 
 
